91f57d14b94b06c877c456ca316ac5d323659936
Kamil Górzyński <kamilgorzynski@gmail.com>
2023-05-08 12:04:32 +0200

    - tłumaczenia - kontroler + lista projektów (bez projektów)

---
+++ src/main/java/wsb/bugtracker/controllers/ProjectController.java
@@ -0,0 +1,20 @@
+package wsb.bugtracker.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/projects")
+public class ProjectController {
+
+    @GetMapping
+    ModelAndView index(){
+        ModelAndView modelAndView = new ModelAndView("projects/index");
+        // TODO: dodać model do widoku
+        return modelAndView;
+
+    }
+
+}
