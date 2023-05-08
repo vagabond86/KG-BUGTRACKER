97c65c58f11f1b37c0bafe78d390334fac994006
Kamil Górzyński <kamilgorzynski@gmail.com>
2023-05-08 09:13:23 +0200

    init

---
+++ src/main/java/wsb/bugtracker/WsbDemoBugtrackerApplication.java
@@ -0,0 +1,13 @@
+package wsb.bugtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsbDemoBugtrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WsbDemoBugtrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/wsb/bugtracker/WsbDemoBugtrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package wsb.bugtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsbDemoBugtrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
