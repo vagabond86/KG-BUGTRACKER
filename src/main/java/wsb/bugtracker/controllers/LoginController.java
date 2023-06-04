09eaf8d41f479e8ec40f295bc03bc66394044a07
Kamil Górzyński <kamilgorzynski@gmail.com>
2023-06-04 21:42:34 +0200

    - logout

--- src/main/java/wsb/bugtracker/controllers/LoginController.java
+++ src/main/java/wsb/bugtracker/controllers/LoginController.java
@@ -1,5 +1,10 @@
 package wsb.bugtracker.controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -8,5 +13,14 @@ public class LoginController {
     @GetMapping("/login")
     public String login(){
         return "security/login";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null) {
+            new SecurityContextLogoutHandler().logout(request, response, authentication);
+        }
+        return "redirect:/login?logout";
     }
 }
