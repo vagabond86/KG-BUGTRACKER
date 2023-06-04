b2f5b7194a893aa67e2a201f33c133c746c352a2
Kamil Górzyński <kamilgorzynski@gmail.com>
2023-06-04 10:34:08 +0200

    - paginacja, - sortowanie, - dodanie ikonek z fontawesome

--- src/main/java/wsb/bugtracker/filters/ProjectFilter.java
+++ src/main/java/wsb/bugtracker/filters/ProjectFilter.java
@@ -3,6 +3,7 @@ package wsb.bugtracker.filters;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.domain.Specification;
 import wsb.bugtracker.models.Person;
 import wsb.bugtracker.models.Project;
@@ -43,6 +44,36 @@ public class ProjectFilter {
             return Specification.where(null);
         }
         return (root, query, criteriaBuilder) -> criteriaBuilder.like(criteriaBuilder.lower(root.get(property)), "%" + value.toLowerCase() + "%");
+    }
+
+    public String toQueryString(Integer page, Sort sort) {
+        return "page=" + page +
+                "&sort=" + toSortString(sort) +
+                (name != null ? "&name=" + name : "") +
+                (creator != null ? "&creator=" + creator.getId() : "") +
+                (globalSearch != null ? "&globalSearch=" + globalSearch : "");
+    }
+
+    public String toSortString(Sort sort) {
+        Sort.Order order = sort.getOrderFor("name");
+        String sortString = "";
+        if (order != null) {
+            sortString += "name," + order.getDirection();
+        }
+        return sortString;
+    }
+
+    public Sort findNextSorting(Sort currentSorting) {
+        Sort.Direction currentDirection = currentSorting.getOrderFor("name") != null ? currentSorting.getOrderFor("name").getDirection() : null;
+
+        if (currentDirection == null) {
+            return Sort.by("name").ascending();
+        } else if(currentDirection.isAscending()) {
+            return Sort.by("name").descending();
+        } else {
+            return Sort.unsorted();
+        }
+
     }
 
 }
