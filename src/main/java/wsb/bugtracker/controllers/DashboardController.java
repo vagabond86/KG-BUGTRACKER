65d13de1a4e855170f3e87c3f908831495ca8c8d
Kamil Górzyński <kamilgorzynski@gmail.com>
2023-06-14 17:31:59 +0200

    - fixes - userRealName w dashboard

--- src/main/java/wsb/bugtracker/controllers/DashboardController.java
+++ src/main/java/wsb/bugtracker/controllers/DashboardController.java
@@ -3,29 +3,42 @@ package wsb.bugtracker.controllers;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 import wsb.bugtracker.models.Issue;
+import wsb.bugtracker.models.Person;
 import wsb.bugtracker.models.Project;
+import wsb.bugtracker.repositories.PersonRepository;
 import wsb.bugtracker.services.IssueService;
 import wsb.bugtracker.services.ProjectService;
 
 import java.util.List;
+import java.util.Optional;
+
 @Controller
 @RequiredArgsConstructor
 public class DashboardController {
 
     private final IssueService issueService;
     private final ProjectService projectService;
+    private final PersonRepository personRepository;
 
     @GetMapping("/dashboard")
-    public ModelAndView dashboard(){
+    public ModelAndView dashboard() {
         ModelAndView modelAndView = new ModelAndView("dashboard");
 
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        Object principal = authentication.getPrincipal();
-        modelAndView.addObject("principal", principal);
+        User user = (User) authentication.getPrincipal();
+
+        Optional<Person> person = personRepository.findByLogin(user.getUsername());
+        if (person.isPresent()) {
+            String userRealName = person.get().getUserRealName();
+            modelAndView.addObject("userRealName", userRealName);
+        }
+
+        modelAndView.addObject("principal", user);
 
         List<Issue> issues = issueService.findAll();
         modelAndView.addObject("issue", issues);
