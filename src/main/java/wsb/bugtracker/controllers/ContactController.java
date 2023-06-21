7b867e128f3b9d6f5d83497e5329320de7ac0809
Kamil Górzyński <kamilgorzynski@gmail.com>
2023-06-21 18:51:11 +0200

    - ukrycie menu przed niezalogowanym użytkownikiem

--- src/main/java/wsb/bugtracker/controllers/ContactController.java
+++ src/main/java/wsb/bugtracker/controllers/ContactController.java
@@ -1,13 +1,26 @@
 package wsb.bugtracker.controllers;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequiredArgsConstructor
 public class ContactController {
 
     @GetMapping("/contact")
-    public String contact() {
-        return "contact";
+    public ModelAndView contact() {
+        boolean loggedIn = isLoggedIn();
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("loggedIn", loggedIn);
+        return modelAndView;
+    }
+
+    private boolean isLoggedIn() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return authentication != null && authentication.isAuthenticated();
     }
 }
--- src/main/java/wsb/bugtracker/controllers/LoginController.java
+++ src/main/java/wsb/bugtracker/controllers/LoginController.java
@@ -8,14 +8,23 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequiredArgsConstructor
 public class LoginController {
 
     @GetMapping("/login")
-    public String login() {
-        return "security/login";
+    public ModelAndView login() {
+        boolean loggedIn = isLoggedIn();
+        ModelAndView modelAndView = new ModelAndView("security/login");
+        modelAndView.addObject("loggedIn", loggedIn);
+        return modelAndView;
+    }
+
+    private boolean isLoggedIn() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return authentication != null && authentication.isAuthenticated();
     }
 
     @GetMapping("/logout")
