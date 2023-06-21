fd6ce4eeb2297a4953d1e134342c31347339b31c
Kamil Górzyński <kamilgorzynski@gmail.com>
2023-06-21 10:55:55 +0200

    - myAccount / change personal data & password - login / readonly - fixes

--- src/main/java/wsb/bugtracker/controllers/MyAccountController.java
+++ src/main/java/wsb/bugtracker/controllers/MyAccountController.java
@@ -7,7 +7,10 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import wsb.bugtracker.models.Person;
 import wsb.bugtracker.repositories.PersonRepository;
@@ -39,13 +42,44 @@ public class MyAccountController {
 
     @GetMapping("/edit")
     @Secured("ROLE_USER")
-    ModelAndView changePersonalData(Authentication authentication) {
+    ModelAndView edit(Authentication authentication) {
         ModelAndView modelAndView = new ModelAndView("/my-account/edit");
         String username = authentication.getName();
         Person person = personRepository.findByLogin(username)
                 .orElseThrow(() -> new RuntimeException("User not found"));
 
         modelAndView.addObject("person", person);
+        return modelAndView;
+    }
+
+    @PostMapping("/update")
+    @Secured("ROLE_USER")
+    ModelAndView update(@ModelAttribute @Valid Person person, BindingResult result, Authentication authentication) {
+        ModelAndView modelAndView = new ModelAndView();
+
+        if (result.hasErrors()) {
+            modelAndView.setViewName("/my-account/edit");
+            modelAndView.addObject("person", person);
+            return modelAndView;
+        }
+
+        String username = authentication.getName();
+        Person currentPerson = personRepository.findByLogin(username)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        currentPerson.setLogin(person.getLogin());
+        currentPerson.setEmail(person.getEmail());
+        currentPerson.setUserRealName(person.getUserRealName());
+
+
+        String hashedPassword = bCryptPasswordEncoder.encode(person.getPassword());
+        currentPerson.setPassword(hashedPassword);
+
+        personService.save(currentPerson);
+
+
+        modelAndView.setViewName("redirect:/my-account");
+
         return modelAndView;
     }
 }
